857f0ff0a97cd50504d2f12e7008682ace1f8f44
surendra <vsurendra775@gmail.com>
2022-07-30 22:13:11 +0530

    change the files

--- Test/src/com/cts/test/Test.java
+++ Test/src/com/cts/test/Test.java
@@ -3,7 +3,21 @@ package com.cts.test;
 public class Test {
 
 	public static void main(String[] args) {
+		
+	int i;
+	int j=12;
+	int z; 
+	for(i=1;i<=10;i++){
+		z=j*i;
+		System.out.println("Show the values: "+j+" * "+i+"  = "+z);
+	}
+		
+		
+		
+		
 		System.out.println("Hello, welcome to java");
+		
+		
 		
 
 	}
